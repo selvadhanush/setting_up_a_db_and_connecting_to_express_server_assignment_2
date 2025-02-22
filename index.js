4f33afe7a96c122f79322d3593d1f1970d850d39
selvadhanush <selva.dhanush.s81@kalvium.community>
2025-02-22 10:50:24 +0530

    done

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const { resolve } = require('path');
+const User =require('./schema');
+const mongoose = require('mongoose');
+require ('dotenv').config();
 
 const app = express();
 const port = 3010;
+app.use(express.json())
 
 app.use(express.static('static'));
 
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>
+  console.log('conneted to database'))
+.catch(err=>
+  console.log('Error connecting to database',err));
+
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+app.post('/api/users', async (req, res) => {
+  try {
+      const { name, email, password } = req.body;
+      
+    
+      const newUser = new User({ name, email, password });
+      await newUser.save();
+
+      res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+      console.error('Error saving user:', error.errors);
+      if(error.name=="ValidationError"){
+       return res.status(400).json({message:`validation error check the key`})
+      }
+      
+      res.status(500).json({ message:error.message });
+  }
 });
 
 app.listen(port, () => {
